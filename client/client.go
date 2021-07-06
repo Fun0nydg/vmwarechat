0dd2a2b4504ef55ce120ecf9cfb68085f4610c35
Fun0nydg <75740174+Fun0nydg@users.noreply.github.com>
2021-07-06 20:57:16 +0800

    Update client.go

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -9,9 +10,16 @@ import (
 )
 
 func main() {
-	var input string
-
-	conn, _ := net.Dial("tcp", "localhost:5000")
+	ip := flag.String("ip", "", "")
+	port := flag.Uint("p", 50001, "")
+	strport := fmt.Sprint(*port)
+	flag.Parse()
+	fmt.Printf("now we connect to server:%s", *ip+":"+strport)
+	conn, err := net.Dial("tcp", *ip+":"+strport)
+	fmt.Println("\nplease input your msg,enter Q is quit:")
+	if err != nil {
+		fmt.Printf("connect error msg:%s", err.Error())
+	}
 	inputRd := bufio.NewReader(os.Stdin)
 	for {
 		input, err := inputRd.ReadString('\n')
