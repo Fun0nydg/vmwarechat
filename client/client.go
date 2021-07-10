9c7baebf58379817792e81ca28eb75d16b6a8071
Fun0nydg <75740174+Fun0nydg@users.noreply.github.com>
2021-07-10 08:20:58 +0800

    Update client.go

--- client/client.go
+++ client/client.go
@@ -10,17 +10,26 @@ import (
 )
 
 func main() {
-	ip := flag.String("ip", "", "")
-	port := flag.Uint("p", 8001, "")
-	flag.Parse()
+	ip := flag.String("ip", "127.0.0.1", "input ip address")
+	port := flag.Uint("p", 50001, "port")
 	strport := fmt.Sprint(*port)
+	flag.Parse()
 	fmt.Printf("now we connect to server:%s", *ip+":"+strport)
 	conn, err := net.Dial("tcp", *ip+":"+strport)
+	defer conn.Close()
+	inputRd := bufio.NewReader(os.Stdin)
+	fmt.Println("\ninput your name:")
+	name, _ := inputRd.ReadString('\n')
+	trimname := strings.Trim(name, "\n")
+	fmt.Println(trimname)
+	conn.Write([]byte("name" + "|" + trimname))
 	fmt.Println("\nplease input your msg,enter Q is quit:")
+
 	if err != nil {
 		fmt.Printf("connect error msg:%s", err.Error())
 	}
-	inputRd := bufio.NewReader(os.Stdin)
+	go clientread(conn)
+
 	for {
 		input, err := inputRd.ReadString('\n')
 		if err != nil {
@@ -32,6 +41,15 @@ func main() {
 			fmt.Println("quit")
 			return
 		}
-		conn.Write([]byte(triminput))
+		conn.Write([]byte("say" + "|" + trimname + "|" + triminput))
 	}
+}
+
+func clientread(conn net.Conn) {
+	data := make([]byte, 512)
+	redata, err := conn.Read(data)
+	if redata == 0 || err != nil {
+		return
+	}
+	fmt.Println(string(data[:redata]))
 }
