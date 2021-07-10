4bb65be06bdb340d907dac6db3283bfc5e5f873a
Fun0nydg <75740174+Fun0nydg@users.noreply.github.com>
2021-07-10 08:21:22 +0800

    Update server.go

--- server/server.go
+++ server/server.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
+
+var connmap map[string]net.Conn = make(map[string]net.Conn)
 
 func main() {
 	fmt.Println("start the server")
-	listener, err := net.Listen("tcp", "1.1.1.1:8000")
+	listener, err := net.Listen("tcp", "127.0.0.1:50001")
 	if err != nil {
 		fmt.Printf("error listen:%s", err.Error())
 	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -22,12 +26,33 @@ func main() {
 }
 func doServerStuff(conn net.Conn) {
 	for {
+		// who := conn.RemoteAddr()
 		buf := make([]byte, 512)
 		len, err := conn.Read(buf)
 		if err != nil {
 			fmt.Println("Error reading", err.Error())
 			return //终止程序
 		}
-		fmt.Printf("Received data: %v\n", string(buf[:len]))
+		msg_str := strings.Split(string(buf[:len]), "|")
+		switch msg_str[0] {
+		case "name":
+			for k, v := range connmap {
+				if k != msg_str[1] {
+					v.Write([]byte("[" + msg_str[1] + "]:" + "join"))
+				}
+			}
+			connmap[msg_str[1]] = conn
+		case "say":
+			for k, v := range connmap {
+				if k != msg_str[1] {
+					v.Write([]byte("[" + msg_str[1] + "]:" + msg_str[2]))
+				}
+			}
+
+		}
+
+		fmt.Println(msg_str)
+		// conn.Write([]byte)
+
 	}
 }
